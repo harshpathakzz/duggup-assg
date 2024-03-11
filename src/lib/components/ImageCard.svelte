ac53107ea99ae672b5cc24d2e4a6b3cdd422c942
harshpathakzz <harshpathak2017@gmail.com>
2024-03-11 21:15:43 +0530

    added

---
+++ src/lib/components/ImageCard.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	import * as Card from '$lib/components/ui/card';
+	export let imageSrc: string;
+	export let altText: string;
+	export let content: string;
+</script>
+
+<div>
+	<Card.Root class="max-w-[350px] rounded-xl">
+		<Card.Header class="m-0 p-0">
+			<img src={imageSrc} alt={altText} class="h-[150px] max-w-[350px] rounded-t-xl object-cover" />
+		</Card.Header>
+		<Card.Content>
+			<p class="mt-5 text-sm font-light text-black">{content}</p>
+		</Card.Content>
+	</Card.Root>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,9 +2,12 @@
 	import Button from '$lib/components/ui/button/button.svelte';
 	import Nav from '$lib/components/ui/nav.svelte';
 	import UserInfo from '$lib/components/UserInfo.svelte';
+	import Timeline from '$lib/components/Timeline.svelte';
+	import TimelineOrgCard from '$lib/components/TimelineOrgCard.svelte';
 </script>
 
 <div>
 	<Nav />
 	<UserInfo />
+	<TimelineOrgCard />
 </div>
